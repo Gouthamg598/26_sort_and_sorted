16a1e556f0e9e9c754f3e25a2965088818f64bd1
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-06-13 15:33:53 +0530

    Add files via upload

---
+++ sort_and_sorted.py
@@ -0,0 +1,14 @@
+'''sort() is used for only lists and sorted is used for list,tuple,set and dictionaries'''
+
+# # a={'z','f','n','q','a','d','f','t' }
+# # a=('z','f','n','q','a','d','f','t' )
+# # a=['z','f','n','q','a','d','f','t' ]
+# a=[5,6,2,2,6,3,31,6,9]
+# print(sorted(a))
+# a.sort(reverse=True)
+# print(a)
+'''---------------------------'''
+#  n = int(input())
+# arr = map(int, input().split())#5 -5 0 5
+# arr=sorted(arr,reverse=True)
+# print(arr)#[5, 5, 0, -5]
